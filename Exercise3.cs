7c71984ecf7c7ccd574e8be2b87e8c7563a0c4cf
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-26 11:57:05 +0200

    Add files via upload

---
+++ Exercise3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise3
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            int y=0;
+            Console.Write(" Input a string: ");
+            x = Console.ReadLine();
+
+            while (y <= x.Length - 1)
+            {
+                Console.Write("{0} ", x[y]);
+                y++;
+            }
+
+            Console.Write(" is the separate stirng.");
+            Console.ReadLine();
+        }
+    }
+}
