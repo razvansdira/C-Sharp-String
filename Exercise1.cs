9f0b5ff97644d9fdfa9903844cc04fb070d98626
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-26 11:47:30 +0200

    Add files via upload

---
+++ Exercise1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            Console.Write(" Input a string: ");
+            x = Console.ReadLine();
+            Console.Write("The string you entered is: {0}", x);
+            Console.ReadLine();
+        }
+    }
+}
