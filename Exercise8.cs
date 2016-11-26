8ddf7bdeffe365450742715c94f6c4b8d6dfa21f
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-26 13:54:41 +0200

    Add files via upload

---
+++ Exercise8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise8
+    {
+        public static void Main(string[] args)
+        {
+            string a;
+            int i=0, j;
+
+
+            Console.Write(" Input a string: ");
+            a = Console.ReadLine();
+            j = a.Length;
+            string[] b = new string[j];
+
+            while (i < j)
+            {
+                string x = a[i].ToString();
+                b[i] = x;
+                i++;
+            }
+            Console.Write("The copied string is: {0}", string.Join("", b));
+            Console.ReadLine();
+        }
+    }
+}
