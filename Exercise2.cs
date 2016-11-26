8dd7d74fae363650e34f0a8418401fe707884ede
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-26 11:51:42 +0200

    Add files via upload

---
+++ Exercise2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            int y=0;
+            Console.Write(" Input a string: ");
+            x = Console.ReadLine();
+
+            foreach (char a in x)
+                y += 1;
+
+            Console.Write("Length of the stirng is: {0}", y);
+            Console.ReadLine();
+        }
+    }
+}
