c431ca153a9e6cedfd6cc256c2befd6b5d6c99a7
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-26 12:25:23 +0200

    Add files via upload

---
+++ Exercise5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise5
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            int a=1, b=0;
+            Console.Write(" Input a string: ");
+            x = Console.ReadLine();
+
+            while (b <= x.Length - 1)
+            {
+                if (x[b] == ' ' || x[b] == '\n')
+                    a++;
+                b++;
+            }
+
+            Console.Write("The number of the stirng is: {0}", a);
+            Console.ReadLine();
+        }
+    }
+}
