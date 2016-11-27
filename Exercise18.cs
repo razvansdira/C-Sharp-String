05c2be9cd30c219248e914c1d0ba54ffb110cb19
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:19:40 +0200

    Add files via upload

---
+++ Exercise18.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise18
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Input a caracter: ");
+            char c = (char)Console.Read();
+
+            if (Char.IsLetter(c))
+            {
+                if (Char.IsUpper(c))
+                    Console.Write("The character is uppercase.");
+                else
+                    Console.Write("The character is lowercase.");
+            }
+            else
+                Console.Write("The character is non-alphabetic.");
+
+            Console.ReadLine();
+        }
+    }
+}
