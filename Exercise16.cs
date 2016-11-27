1b9f872db67fca105b4d4bf7c2e54929c8e322ba
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:18:01 +0200

    Add files via upload

---
+++ Exercise16.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise16
+    {
+        static void Main(string[] args)
+        {
+            string user, pass;
+            int a = 0;
+
+            do
+            {
+                Console.Write("Input a username: ");
+                user = Console.ReadLine();
+
+                Console.Write(" Input a password: ");
+                pass = Console.ReadLine();
+
+                if (user != "razvan" || pass != "asd123!@#")
+                    a++;
+                else
+                    a = 1;
+            }
+            while ((user != "razvan" || pass != "asd123!@#") && (a != 3));
+
+            if (a == 3)
+                Console.Write("Try later !!!");
+            else
+                Console.Write("Password entered successfull !!!");
+            Console.ReadLine();
+        }
+    }
+}
