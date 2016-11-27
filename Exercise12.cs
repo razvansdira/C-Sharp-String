f4a2ed497a8dbe3455df8d771df5848b87d61543
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:13:16 +0200

    Add files via upload

---
+++ Exercise12.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise12
+    {
+        static void Main(string[] args)
+        {
+            string[] arr;
+            string x;
+            int a, b, c, d;
+
+            Console.Write("  Input number of string: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            arr = new string[a];
+            Console.WriteLine(" Input {0} strings below: ", a);
+
+            for (b = 0; b < a; b++)
+                arr[b] = Console.ReadLine();
+            d = arr.Length;
+
+            for(b=0; b<d; b++)
+            {
+                for(c=0; c<d-1; c++)
+                {
+                    if (arr[c].CompareTo(arr[c + 1]) > 0)
+                    {
+                        x = arr[c];
+                        arr[c] = arr[c + 1];
+                        arr[c + 1] = x;
+                    }
+                }
+            }
+            Console.Write(" After sorting: ");
+            for (b = 0; b < d; b++)
+                Console.Write(arr[b] + " ");
+            Console.ReadLine();
+        }
+    }
+}
