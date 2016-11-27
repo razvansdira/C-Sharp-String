7804a4ecf4f11d06ec1d576662c141f536c155a9
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:16:52 +0200

    Add files via upload

---
+++ Exercise15.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise15
+    {
+        static void Main(string[] args)
+        {
+            string x;
+            char[] arr;
+            char c;
+            int i, l = 0;
+
+            Console.Write(" Input a string: ");
+            x = Console.ReadLine();
+            l = x.Length;
+            arr = x.ToCharArray(0, l);
+
+            Console.Write("After conversion: ");
+            for (i = 0; i < l; i++)
+            {
+                c = arr[i];
+                if (Char.IsLower(c))
+                    Console.Write(Char.ToUpper(c));
+                else
+                    Console.Write(Char.ToLower(c));
+            }
+            Console.ReadLine();
+        }
+    }
+}
