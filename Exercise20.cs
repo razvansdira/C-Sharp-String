8124d43f5d8bf75cd9e4d088f4dabaa57b5ca0b4
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:45:00 +0200

    Add files via upload

---
+++ Exercise20.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise20
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c;
+            int n;
+
+            Console.Write(" Input a string: ");
+            a = Console.ReadLine();
+
+            Console.Write("Input the strinng to be searched for: ");
+            b = Console.ReadLine();
+
+            Console.Write(" Input the string to be inserted: ");
+            c = Console.ReadLine();
+
+            n = a.IndexOf(b);
+            c = "" + c.Trim() + " ";
+            a = a.Insert(n, c);
+
+            Console.Write("The final string is: {0}.", a);
+            Console.ReadLine();
+         }
+    }
+}
