95f4f5c19079135560824aae24908bbba341f96b
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:15:12 +0200

    Add files via upload

---
+++ Exercise14.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise14
+    {
+        static void Main(string[] args)
+        {
+            string a, b;
+            bool x;
+
+            Console.Write(" Input a string: ");
+            a = Console.ReadLine();
+
+            Console.Write("Input the substring to be search: ");
+            b = Console.ReadLine();
+
+            x = a.Contains(b);
+            if (x)
+                Console.Write(" The substring exists in the string.");
+            else
+                Console.Write(" The substring is not exists in the string.");
+            Console.ReadLine(); ;
+        }
+    }
+}
