32f1cbec239c8563793504b4ff9c6d699ddcd254
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:18:54 +0200

    Add files via upload

---
+++ Exercise17.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise17
+    {
+        static void Main(string[] args)
+        {
+            string x, y;
+            Console.Write(" Input a string: ");
+            x = Console.ReadLine();
+
+            Console.Write("Input a string to be found in the string: ");
+            y = Console.ReadLine();
+
+            int n = x.IndexOf(y);
+            if (n < 0)
+                Console.Write("Not found.");
+            else
+                Console.Write("Found '{0}' in '{1}' at position {2}. ", y, x, n);
+            Console.ReadLine();
+        }
+    }
+}
