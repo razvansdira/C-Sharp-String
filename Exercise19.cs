94a8cf90f7696892233d4b230bcf57abafb48ea1
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-27 10:11:02 +0200

    Add files via upload

---
+++ Exercise19.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise19
+    {
+        static void Main (string[] args)
+        {
+            string x, y;
+            int a = 0, b = -1, c = -1;
+
+            Console.Write("  Input a string: ");
+            x = Console.ReadLine();
+            Console.Write("Input the string to be searched for: ");
+            y = Console.ReadLine();
+
+            while (a != -1)
+            {
+                a = x.IndexOf(y, c + 1);
+                b += 1;
+                c = a;
+            }
+            Console.Write(" The string '{0}' occurs " + b + " times.", y);
+            Console.ReadLine();
+        }
+    }
+}
