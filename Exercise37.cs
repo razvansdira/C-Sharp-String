26379a25b643317988e71eb6fe3cbf7c5ef5fc41
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-28 22:34:25 +0200

    Add files via upload

---
+++ Exercise37.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise37
+    {
+        static void Main(string[] args)
+        {
+            string[] a = { "hei ", "welcome ", "to ", "create ", "C# ", "client ", "app " };
+            Console.WriteLine(string.Concat(a));
+
+            Array.Sort(a);
+            Console.Write(string.Concat(a));
+            Console.ReadLine();
+        }
+    }
+}
