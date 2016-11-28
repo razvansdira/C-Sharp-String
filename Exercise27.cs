249daca56a95e4d46e8e69595d87df52217a2475
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-28 10:06:10 +0200

    Add files via upload

---
+++ Exercise27.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace C_Sharp_String
+{
+    class Exercise27
+    {
+        static void Mai(string[] args)
+        {
+            String a = "change";
+            String b = "euro";
+            String n = null;
+
+            n = symbol(String.Compare(a, b, false, new CultureInfo("en-US")));
+            Console.WriteLine("  For US: {0} {1} {2}", a, n, b);
+
+            n = symbol(String.Compare(a, b, false, new CultureInfo("cs-CZ")));
+            Console.WriteLine("  For CZ: {0} {1} {2}\n", a, n, b);
+
+            Console.ReadLine();
+        }
+
+        private static String symbol(int x)
+        {
+            String s = "=";
+            if (x < 0) s = "<";
+            else if (x > 0) s = ">";
+            return s;
+        }  
+    }
+}
