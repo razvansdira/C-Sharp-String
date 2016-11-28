18654492f09f5694254def05eddb5cc3ae377a75
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-28 10:08:37 +0200

    Add files via upload

---
+++ Exercise30.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Globalization;
+/*
+namespace C_Sharp_String
+{
+    class Exercise30
+    {
+        static void Main(String[] args)
+        {
+            String low = "xyz";
+            String up = "XYZ";
+            String r = "equal to ";
+            int x = 0, n = 1;
+
+            x = String.CompareOrdinal(low, n, up, n, 1);
+            if (x < 0)
+                r = "less than";
+            if (x > 0)
+                r = "greater than";
+
+            Console.WriteLine(" Compare ordinal(\"{0}\"[{2}], \"{1}\"[{2}]):", low, up, n);
+            Console.WriteLine("     '{0}' is {1} '{2}'", low[n], r, up[n]);
+
+            x = String.Compare(low, n, up, n, 1, false, new CultureInfo("en-US"));
+            if (x < 0)
+                r = "less than";
+            else if (x > 0)
+                r = "greater than";
+
+            Console.WriteLine("  Compare(\"{0}\"[{2}], \"{1}\"[{2}]):", low, up, n);
+            Console.WriteLine("     '{0}' is {1} '{2}'", low[n], r, up[n]);
+            Console.ReadLine();
+        } 
+    }
+}
+*/
