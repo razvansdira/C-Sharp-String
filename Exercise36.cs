ce2eb4416b83f1ecb497a9a9476a509b28b9ef1f
Razvan Sdira <sdira_razvan@yahoo.com>
2016-11-29 09:50:17 +0200

    Add files via upload

---
+++ Exercise36.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_String
+{
+    class Exercise36
+    {
+        static void Main(string[] args)
+        {
+            string a = "Ana ";
+            string b = "are ";
+            string c = "mere.";
+
+            var d = string.Concat(a, b, c);
+            Console.Write(d);
+            Console.ReadLine();
+        }
+    }
+}
